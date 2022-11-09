7587245d02367b34172383e1753df62120235aad
dhollander8991 <dhollander8991@gmail.com>
2022-11-09 10:46:25 +0200

    added header photo

--- src/root.tsx
+++ src/root.tsx
@@ -22,6 +22,8 @@ export default component$(() => {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6156534121952969"
           crossOrigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossOrigin="anonymous"></script>
+
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body lang="en">
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -2,11 +2,15 @@ import { component$, Slot } from '@builder.io/qwik';
 import Header from '../components/header/header';
 import LeftSidebar from '../components/left-side-bar/left-sidebar';
 import RightSidebar from '../components/right-side-bar/right-sidebar';
+import Footer from '../components/footer/footer';
 export default component$(() => {
   return (
     <>
       <main>
         <Header />
+        <div class="image-header">
+          <img src="./images/header/header.png" style="width: 100vw;" />
+        </div>
         <div class="main-container">
           <LeftSidebar />
           <Slot />
@@ -15,8 +19,7 @@ export default component$(() => {
         <section>
         </section>
       </main>
-      <footer>
-      </footer>
+      <Footer />
     </>
   );
 });
